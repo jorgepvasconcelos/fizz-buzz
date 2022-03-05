6b05c05647b4f5eab4bbfdd4b42bb6fbfcbe79ec
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-05 11:19:10 -0300

    test: add test_when_fizzbuzz_not_receives_int_value_should_raise_valueerror

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -1,3 +1,4 @@
+from pytest import mark
 from fizz_buzz.code import fizz_buzz
 
 
@@ -11,3 +12,12 @@ def test_when_fizzbuzz_receives_multiples_of_3_should_return_fizz():
 
 def test_when_fizzbuzz_receives_multiples_of_5_should_return_buzz():
     assert fizz_buzz(value=5) == 'buzz'
+
+
+def test_when_fizzbuzz_not_receives_multiples_of_3_and_5_should_return_itself_value():
+    assert fizz_buzz(value=4) == 4
+
+
+@mark.xfail()
+def test_when_fizzbuzz_not_receives_int_value_should_raise_valueerror():
+    assert fizz_buzz(value='invalid') == 4
