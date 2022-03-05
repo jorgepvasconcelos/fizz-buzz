a340607ea1c600a1fda5e2bc4ccae872f33fc840
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-05 11:40:48 -0300

    test: use mark.parametrize()

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -2,20 +2,24 @@ from pytest import mark
 from fizz_buzz.code import fizz_buzz
 
 
-def test_when_fizzbuzz_receives_multiples_of_3_and_5_should_return_fizzbuzz():
-    assert fizz_buzz(value=15) == 'fizzbuzz'
+@mark.parametrize('input_value', [15, 30, 45, 60])
+def test_when_fizzbuzz_receives_multiples_of_3_and_5_should_return_fizzbuzz(input_value):
+    assert fizz_buzz(value=input_value) == 'fizzbuzz'
 
 
-def test_when_fizzbuzz_receives_multiples_of_3_should_return_fizz():
-    assert fizz_buzz(value=3) == 'fizz'
+@mark.parametrize('input_value', [3, 6, 9, 12])
+def test_when_fizzbuzz_receives_multiples_of_3_should_return_fizz(input_value):
+    assert fizz_buzz(value=input_value) == 'fizz'
 
 
-def test_when_fizzbuzz_receives_multiples_of_5_should_return_buzz():
-    assert fizz_buzz(value=5) == 'buzz'
+@mark.parametrize('input_value', [5, 10, 20, 25])
+def test_when_fizzbuzz_receives_multiples_of_5_should_return_buzz(input_value):
+    assert fizz_buzz(value=input_value) == 'buzz'
 
 
-def test_when_fizzbuzz_not_receives_multiples_of_3_and_5_should_return_itself_value():
-    assert fizz_buzz(value=4) == 4
+@mark.parametrize('input_value', [1, 2, 43, 74])
+def test_when_fizzbuzz_not_receives_multiples_of_3_and_5_should_return_itself_value(input_value):
+    assert fizz_buzz(value=input_value) == input_value
 
 
 @mark.xfail()
