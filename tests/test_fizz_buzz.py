f3b4519283ab6ebebed77cb63d8e0b3d8ee16001
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-05 11:04:41 -0300

    test: add test_when_fizzbuzz_receives_multiples_of_3_should_return_fizz

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -1,5 +1,9 @@
 from fizz_buzz.code import fizz_buzz
 
 
+def test_when_fizzbuzz_receives_multiples_of_3_should_return_fizz():
+    assert fizz_buzz(value=3) == 'fizz'
+
+
 def test_when_fizzbuzz_receives_multiples_of_5_should_return_buzz():
     assert fizz_buzz(value=5) == 'buzz'
