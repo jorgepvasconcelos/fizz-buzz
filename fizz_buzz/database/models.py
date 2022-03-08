4f82b9ca9149d9fa8650e58b23969543ebd1b2e5
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-08 00:32:06 -0300

    feat: engines

--- fizz_buzz/database/models.py
+++ fizz_buzz/database/models.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# url = f'mysql://{user_name}:{password}@{host}:{port}/{schema_name}'
+engine = create_engine('mysql://root:MySql2019!@localhost:3306/testedb', echo=False)
 engine = create_engine('sqlite:///teste.db', echo=False)
 Base = declarative_base()
 
