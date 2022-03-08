a8d63a75f02668e621ddbee892ba7bb594982fa7
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-08 09:28:33 -0300

    feat: users route

--- fizz_buzz/app.py
+++ fizz_buzz/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+
 from fizzbuzz_resolver import fizzbuzz_resolver
 from validation import validation_fizzbuzz_route
+from database.helpers import create_session
+from database.models import User
 
 app = Flask(__name__)
 
@@ -21,5 +24,16 @@ def index():
     return 'I am alive'
 
 
+@app.route('/users', methods=['GET'])
+def users():
+    with create_session() as session:
+        config = {'age': '10'}
+        print(config)
+        results = session.query(User).all()
+        print(results)
+
+    return 'I am alive'
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
