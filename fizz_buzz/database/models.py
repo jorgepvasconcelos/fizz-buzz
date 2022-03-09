bfe0e1a932005af647b2b3bd48afa14efb0e493d
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-09 09:05:29 -0300

    feat: change root password

--- fizz_buzz/database/models.py
+++ fizz_buzz/database/models.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 # url = f'mysql://{user_name}:{password}@{host}:{port}/{schema_name}'
-engine = create_engine('mysql://root:123@mysql_server:3306/testedb', echo=False)
-# engine = create_engine('sqlite:///teste.db', echo=False)
+# engine = create_engine('mysql://root:123@mysql_server:3306/testedb', echo=False)
+engine = create_engine('sqlite:///teste.db', echo=False)
 Base = declarative_base()
 
 
